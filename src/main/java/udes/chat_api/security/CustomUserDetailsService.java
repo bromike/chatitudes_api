0c1c310793c1353dd33b960e7ea245763a91bf73
Steeven Bosse <Steeven.Bosse@usherbrooke.ca>
2018-06-12 17:19:25 -0400

    Add the ability to valid CAS ticket from an external service, such as the chat dashboard.

--- src/main/java/udes/chat_api/security/CustomUserDetailsService.java
+++ src/main/java/udes/chat_api/security/CustomUserDetailsService.java
@@ -2,10 +2,9 @@ package udes.chat_api.security;
 
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.Set;
+import java.util.Map;
 
 public class CustomUserDetailsService implements AuthenticationUserDetailsService<CasAssertionAuthenticationToken> {
     public CustomUserDetailsService() {
@@ -13,7 +12,13 @@ public class CustomUserDetailsService implements AuthenticationUserDetailsServic
     }
 
     @Override
-    public UserDetails loadUserDetails(CasAssertionAuthenticationToken casAssertionAuthenticationToken) throws UsernameNotFoundException {
-        return new AppUserDetails(casAssertionAuthenticationToken.getPrincipal().toString(), casAssertionAuthenticationToken.getName());
+    public AppUserDetails loadUserDetails(CasAssertionAuthenticationToken casAssertionAuthenticationToken) throws UsernameNotFoundException {
+        Map<String, Object> properties = casAssertionAuthenticationToken.getAssertion().getPrincipal().getAttributes();
+        AppUserDetails userDetails = new AppUserDetails();
+        userDetails.setCip((String)properties.get("cip"));
+        userDetails.setCourriel((String)properties.get("courriel"));
+        userDetails.setFirst_name((String)properties.get("prenom"));
+        userDetails.setLast_name((String)properties.get("nomFamille"));
+        return userDetails;
     }
 }
